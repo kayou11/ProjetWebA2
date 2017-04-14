522da8755fa8ff975e55da840b197a20de925842
kayou11 <p.kerebelomer@gmail.com>
2017-04-14 23:49:24 +0200

    Ajout de SyliusBundle pour la boutique

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,7 +23,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\RestBundle\FOSRestBundle(),
-            new Aimeos\ShopBundle\AimeosShopBundle(),
             new AppBundle\AppBundle()
         ];
 
